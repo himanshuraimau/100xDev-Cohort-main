7e6c38b99edd71556c033b6c0ffa6e550198fde7
Himanshu Rai <himanshuraimau9@gmail.com>
2024-06-06 23:41:24 +0530

    add next-auth

---
+++ week16-nextauth/src/app/api/auth/[..nextauth]/routes.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+
+export function GET(req:NextRequest,{params:{authRoutes}}:{
+    params:{
+        authRoutes: string[]
+    }
+}){
+
+    return NextRequest 
+    
+}
