923340f108254edc7c9578e8fa24ff4b0351e551
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-17 14:03:51 +0530

    add redis

--- week19-webSockets/backend/src/index.ts
+++ week19-webSockets/backend/src/index.ts
@@ -1,17 +1,27 @@
+// Importing WebSocket and WebSocketServer from the 'ws' library
 import WebSocket, { WebSocketServer } from 'ws';
+// Importing the 'http' module from Node.js
 import http from 'http';
 
+// Creating an HTTP server
 const server = http.createServer(function(request: any, response: any) {
+    // Logging the received request URL to the console
     console.log((new Date()) + ' Received request for ' + request.url);
+    // Sending a response back to the client
     response.end("hi there");
 });
 
+// Creating a WebSocket server instance, attached to the HTTP server
 const wss = new WebSocketServer({ server });
 
+// Handling WebSocket connections
 wss.on('connection', function connection(ws) {
+  // Handling WebSocket errors
   ws.on('error', console.error);
 
+  // Handling incoming WebSocket messages
   ws.on('message', function message(data, isBinary) {
+    // Sending received message to all connected clients
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
@@ -19,9 +29,11 @@ wss.on('connection', function connection(ws) {
     });
   });
 
+  // Sending a welcome message to the newly connected client
   ws.send('Hello! Message From Server!!');
 });
-
+// Listening on port 8080 for incoming HTTP and WebSocket connections
 server.listen(8080, function() {
+    // Logging that the server is now listening on port 8080
     console.log((new Date()) + ' Server is listening on port 8080');
 });
